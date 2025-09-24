12f16c134e5024cc0cd695fe916c79338deb1b16
Leo Carvalho <25367871+thisisleobro@users.noreply.github.com>
2025-09-24 10:00:50 -0100

    fix: add proper support for subsystem:windows when using msvc

---
+++ src/WinMain.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv);
+
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	int argc;
+	LPWSTR *argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+	char **argv = (char **)malloc(sizeof(char *) * argc);
+
+	for (int i = 0; i < argc; i++) {
+		int len = WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, NULL, 0, NULL, NULL);
+		argv[i] = (char *)malloc(len);
+		WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, argv[i], len, NULL, NULL);
+	}
+
+	printf("calling main from WinMain");
+	int result = main(argc, argv);
+
+	// Cleanup
+	for (int i = 0; i < argc; i++) {
+		free(argv[i]);
+	}
+	free(argv);
+	LocalFree(argvW);
+
+	return result;
+}
